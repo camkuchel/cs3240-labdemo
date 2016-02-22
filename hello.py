192d9a0e012b4b45b52f863344f8cc64040d9a30
Cameron Kuchel <cck4mn@virginia.edu>
2016-02-22 16:02:35 -0500

    Inital commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+__author__ = 'cck4mn'
+# Cameron Kuchel  (cck4mn)
+# 2/22/16
+# CS 3240 Lab 5
+
+def test1():
+
+    s = "Hello"
+    
+if __name__ == "__main__":
+
+    print("hello")
+
+
+
+
+
+
+
